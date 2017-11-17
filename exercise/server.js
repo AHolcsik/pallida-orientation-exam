4aa767c32117218a659dbfc5018874d88d894b19
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-17 10:03:22 +0100

    feat(exam): basic build for server made

---
+++ exercise/server.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const mysql = require('mysql');
+const express = require('express');
+const app = express();
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'imanoodle',
+    database: 'exam'
+  });
+  
+  connection.connect(function(err) {
+    if(err){
+      console.log('Error connecting to database oooh');
+      return;
+    }
+    console.log('Connection established woooo');
+  });
+
+
+app.use(express.json())
+// app.use('/', express.static('./assets'))
+
+// app.get('/playlists', function(req, res) {
+//     connection.query('SELECT * FROM playlists;', function(err, result) {
+//         if(err) {
+//             console.log(err.toString());
+//           }
+//           res.json(result);
+//     })
+// })
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');    
+});
+
+app.listen(8080, function() {
+    console.log('server is up on port 8080, good to go!')
+})
+
