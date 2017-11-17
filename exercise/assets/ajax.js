f163347d68ebe9ab3cab8da8aa693356e49f02ae
AHolcsik <annamaria.holcsik@gmail.com>
2017-11-17 10:07:37 +0100

    feat(exam): ajax.js added

---
+++ exercise/assets/ajax.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+function ajax(method, url, callback) {
+    let xhr = new XMLHttpRequest()
+    xhr.open(method, url)
+    xhr.setRequestHeader('Content-Type', 'application/json')
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            let recievedData = JSON.parse(xhr.responseText)
+            callback(recievedData)
+        }
+    }
+    xhr.send()
+}
